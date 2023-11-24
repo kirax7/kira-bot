d5fc810f1140c3993bf49299bd5f808e6d54cfb0
kirax7 <147973333+kirax7@users.noreply.github.com>
2023-11-24 22:07:25 +0400

    Update ownerContact.js

--- src/configs/ownerContact.js
+++ src/configs/ownerContact.js
@@ -1,8 +1,8 @@
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Meu Dono\n' // full name
-            + 'ORG:Anny Bot;\n' // the organization of the contact
-            + 'TEL;type=CELL;type=VOICE;waid=559887583208:+55 98 8758-3208\n' // WhatsApp ID + phone number
+            + 'ORG:Kira Bot;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=9188817577:+91 918881 7577\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 
 module.exports = vcard
